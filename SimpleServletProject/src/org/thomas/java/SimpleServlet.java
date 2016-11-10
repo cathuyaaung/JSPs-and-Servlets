93fdcd9400e2e352b721cfbf70132a2e8751cdbb
cizrage <thu.ya.a@gmail.com>
2016-11-10 22:53:45 +0800

    04 - Servlet XML Configuration

---
+++ SimpleServletProject/src/org/thomas/java/SimpleServlet.java
@@ -0,0 +1,33 @@
+package org.thomas.java;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet(description = "A simple servlet", urlPatterns = { "/SimpleServletPath3" })
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Hello from GET method");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("<h1>Hello, I'm in HTML 3.</h1>");
+	}
+
+}
