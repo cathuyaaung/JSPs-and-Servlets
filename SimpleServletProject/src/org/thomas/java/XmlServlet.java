2d177bdbccc1d1d1449d4ce2751580ea81388e5e
cizrage <thu.ya.a@gmail.com>
2016-11-11 20:19:07 +0800

    08 - Request, Session and Context

--- SimpleServletProject/src/org/thomas/java/XmlServlet.java
+++ SimpleServletProject/src/org/thomas/java/XmlServlet.java
@@ -3,27 +3,47 @@ package org.thomas.java;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class XmlServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException {
 		resp.setContentType("text/html");
 		PrintWriter out = resp.getWriter();
-		
 		String username = req.getParameter("username");
 		String firstname = req.getParameter("firstname");
 		String lastname = req.getParameter("lastname");
+		out.printf("<br>Hello from GET %s %s %s", username, firstname, lastname);
 		
-		out.printf("Hello from GET %s %s %s", username, firstname, lastname);
+		HttpSession session = req.getSession();
+		ServletContext context  = req.getServletContext();
+		
+		if (username!="" && username !=null) {
+			session.setAttribute("username", username);
+			context.setAttribute("username", username);
+		}	
+		out.printf("<br>Saved in session: username: %s", session.getAttribute("username"));
+		out.printf("<br>Saved in context: username: %s", context.getAttribute("username"));
+				
+		
 	}
 	
 
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
 		resp.setContentType("text/html");
 		PrintWriter out = resp.getWriter();
 		
