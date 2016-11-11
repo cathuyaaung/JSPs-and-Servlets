4f4360a324879ce8dbf8b514d69420a00c94bc1b
cizrage <thu.ya.a@gmail.com>
2016-11-11 23:25:12 +0800

    Update

--- LoginApp/src/org/thomas/java/service/LoginService.java
+++ LoginApp/src/org/thomas/java/service/LoginService.java
@@ -2,7 +2,7 @@ package org.thomas.java.service;
 
 public class LoginService {
 	public static boolean authenticate(String username, String password) {
-		if (username.equals("thuya") && password.equals("ygmfromthuya")) {
+		if (username.equals("thuya") && password.equals("xxx")) {
 			System.out.println("Login successful");
 			return true;
 		}
